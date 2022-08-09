8407f163413b792dcd87e589f2590fc90a14fb4f
Abhishekjadhavv <abhishekjadhav4553@gmail.com>
2022-08-09 21:04:31 +0530

    update

--- script.js
+++ script.js
@@ -5,19 +5,19 @@ let message_btn = document.querySelector(".message-btn");
 let pop_up_box = document.querySelector(".pop-up-box");
 
 // ------- bar js-----
-bar.addEventListener("click",()=>{
-   header.classList.toggle("active");
-   bar.classList.toggle("bx-x");
+bar.addEventListener("click", () => {
+  header.classList.toggle("active");
+  bar.classList.toggle("bx-x");
 });
 
 // ------- bar js end -------
 
 
 // ------- pop_up_box js ------
-message_btn.addEventListener("click",(e)=>{
-   e.preventDefault();
-   pop_up_box.classList.add("active");
-   
+message_btn.addEventListener("click", (e) => {
+  e.preventDefault();
+  pop_up_box.classList.add("active");
+
 });
 
 setInterval(() => {
@@ -29,17 +29,17 @@ setInterval(() => {
 
 
 for (const icon of icons) {
-    icon.addEventListener("click",()=>{
-      document.querySelector(".menu-item .active").classList.remove("active");
-      icon.classList.add("active");
-    });
+  icon.addEventListener("click", () => {
+    document.querySelector(".menu-item .active").classList.remove("active");
+    icon.classList.add("active");
+  });
 }
 
 
-document.querySelector(".Download_Cv").addEventListener("click",()=>{
+document.querySelector(".Download_Cv").addEventListener("click", () => {
   let aTag = document.createElement("a");
   aTag.href = "img/abhishek.pdf";
-  aTag.download =  "img/abhishek.pdf";
+  aTag.download = "img/abhishek.pdf";
   document.body.appendChild(aTag);
   aTag.click();
   aTag.remove();
@@ -47,7 +47,37 @@ document.querySelector(".Download_Cv").addEventListener("click",()=>{
 
 
 // ------- onscroll js ----
-window.onscroll = (()=>{
+window.onscroll = (() => {
   header.classList.remove("active");
-   bar.classList.remove("bx-x");
+  bar.classList.remove("bx-x");
 })
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  slidesPerGroup: 3,
+  grabCursor: true,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    200: {
+      slidesPerView: 1,
+      slidesPerGroup: 1,
+    },
+
+    600: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+      slidesPerGroup: 2,
+    },
+    900: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+      slidesPerGroup: 3,
+    },
+  },
+});
